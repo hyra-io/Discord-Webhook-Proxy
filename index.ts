dfb385ac5313f9ca35d674a5fbad13d90c2451ee
Sam512 <33881069+Sam512@users.noreply.github.com>
2022-03-29 21:48:52 +0100

    Added support for Hyra's Multi-IP Instances to allow for better proxy management

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ All the best!
 import dotenv from 'dotenv';
 dotenv.config();
 
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import MongoStore from 'rate-limit-mongo';
@@ -21,12 +21,46 @@ import mongoose from 'mongoose';
 import { webhooks } from './models/webhooks';
 import { requests } from './models/requests';
 import bodyParser from 'body-parser';
+import { networkInterfaces } from 'os';
+import https from 'https';
 
-const Axios = axios.create({
-    headers: {
-        Via: "HyraWebhookProxy/2.0"
+const nets = networkInterfaces();
+const addresses = [];
+
+for(const name of Object.keys(nets)) {
+    for(const net of nets[name]!) {
+        if(net.family === 'IPv4' && !net.internal) {
+            addresses.push(net.address);
+        }
     }
-})
+}
+
+const axiosInstances: AxiosInstance[] = []
+
+for(let address of addresses) {
+    axiosInstances.push(axios.create({
+        httpsAgent: new https.Agent({
+            localAddress: address
+        }),
+        headers: {
+            Via: "HyraWebhookProxy/2.0"
+        }
+    }))
+}
+
+let instance = 1;
+
+const roundRobinInstance = (): AxiosInstance => {
+    if(instance === axiosInstances.length - 1) {
+        instance = 0;
+        console.log(instance);
+        return axiosInstances[instance];
+    } else {
+        instance++;
+        console.log(instance);
+        return axiosInstances[instance - 1];
+    }
+}
 
 const app = express();
 
@@ -102,7 +136,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/webhooks/:id/:token", limiter, (req, res) => {
     handleCounter(req);
-    Axios.get(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}`).then(result => {
+    roundRobinInstance().get(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}`).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
@@ -112,7 +146,7 @@ app.get("/api/webhooks/:id/:token", limiter, (req, res) => {
 
 app.post("/api/webhooks/:id/:token", limiter, (req, res) => {
     handleCounter(req);
-    Axios.post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}`, req.body).then(result => {
+    roundRobinInstance().post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}`, req.body).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
@@ -122,7 +156,7 @@ app.post("/api/webhooks/:id/:token", limiter, (req, res) => {
 
 app.patch("/api/webhooks/:id/:token/messages/:messageId", limiter, (req, res) => {
     handleCounter(req);
-    Axios.patch(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/messages/${req.params.messageId}`, req.body).then(result => {
+    roundRobinInstance().patch(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/messages/${req.params.messageId}`, req.body).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
@@ -132,7 +166,7 @@ app.patch("/api/webhooks/:id/:token/messages/:messageId", limiter, (req, res) =>
 
 app.delete("/api/webhooks/:id/:token/messages/:messageId", limiter, (req, res) => {
     handleCounter(req);
-    Axios.delete(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/messages/${req.params.messageId}`).then(result => {
+    roundRobinInstance().delete(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/messages/${req.params.messageId}`).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
@@ -142,7 +176,7 @@ app.delete("/api/webhooks/:id/:token/messages/:messageId", limiter, (req, res) =
 
 app.post("/api/webhooks/:id/:token/slack", limiter, (req, res) => {
     handleCounter(req);
-    Axios.post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/slack`, req.body).then(result => {
+    roundRobinInstance().post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/slack`, req.body).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
@@ -152,7 +186,7 @@ app.post("/api/webhooks/:id/:token/slack", limiter, (req, res) => {
 
 app.post("/api/webhooks/:id/:token/github", limiter, (req, res) => {
     handleCounter(req);
-    Axios.post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/github`, req.body).then(result => {
+    roundRobinInstance().post(`https://discord.com/api/webhooks/${req.params.id}/${req.params.token}/github`, req.body).then(result => {
         handleResponse(req, res, result);
     }).catch(err => {
         res.status(err.response.status);
