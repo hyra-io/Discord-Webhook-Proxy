94cd904bef0c1f54209aa09acd32eff754f4279f
Sam512 <33881069+Sam512@users.noreply.github.com>
2022-03-29 22:52:27 +0100

    Add another catch

--- index.ts
+++ index.ts
@@ -128,6 +128,10 @@ const validateRequest = (req: express.Request, res: express.Response) => {
                 code: 50006
             })
             return false;
+        } if(req.body.content && req.body.content.length > 2000) {
+            res.status(400).send({
+                message: "Content must be 2000 or fewer in length."
+            })
         } else if (req.body.embeds && req.body.embeds.length === 0) {
             res.status(400).send({
                 message: "Cannot send an empty message",
